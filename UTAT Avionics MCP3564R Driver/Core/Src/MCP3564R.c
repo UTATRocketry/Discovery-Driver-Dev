09415ba9276d4846891162ab738589b79a4d58be
WilliamGomez1 <180569402+WilliamGomez1@users.noreply.github.com>
2024-11-21 20:03:19 -0500

    Update ReadVoltage function

--- UTAT Avionics MCP3564R Driver/Core/Src/MCP3564R.c
+++ UTAT Avionics MCP3564R Driver/Core/Src/MCP3564R.c
@@ -9,12 +9,13 @@
 
 SPI_HandleTypeDef* MCP3564_hspi1;
 
+
 //Initializes MCP3564 on a particular SPI bus, returns 0 if successful, 1 if failed
 int MCP3564_Init(SPI_HandleTypeDef* hspi1){
 	MCP3564_hspi1 = hspi1;
 
-    uint8_t data;
     HAL_StatusTypeDef status;
+
 // this is a comment
 }
 
@@ -24,19 +25,20 @@ int MCP3564_CheckConnection(){
 
 }
 
-// Reads voltage calculated from MCP3564, returns voltage in V
-float MCP3564_ReadVoltage(){
+// Reads voltage calculated from MCP3564, returns 0 if successful, 1 if failed
+int MCP3564_ReadChannel(int32_t *channelReading){
 
-	uint8_t data[3];
-	HAL_StatusTypeDef status; //returns 0 if no problem
 
+	HAL_StatusTypeDef status;
+	int8_t data[3];
+
+	//returns 0 if no problem
 	status = HAL_SPI_Receive (MCP3564_hspi1, data, 3, 1000);
 
+	//stitches the 3 bytes of data together
+	//todo: verify that these bits are being stitched together correctly
+	*channelReading = ((int32_t)data[0] << 16 | (int32_t)data[1] << 8 | (int32_t)data[2]);
 
-	int32_t raw_voltage = (int16_t)(data[0] << 8 | data[1]);
-
-	//implement conversion from int to float
-
-	return raw_voltage;
+	return status;
 }
 
