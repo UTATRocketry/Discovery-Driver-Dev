cdf592993f7c46a7f63e1b0ebc3f1e2bee61c436
Julianne Deva <julianne.deva@mail.utoronto.ca>
2024-11-29 13:52:08 -0500

    Start CheckConnection Function

--- UTAT Avionics MCP3564R Driver/Core/Src/MCP3564R.c
+++ UTAT Avionics MCP3564R Driver/Core/Src/MCP3564R.c
@@ -22,6 +22,26 @@ int MCP3564_Init(SPI_HandleTypeDef* hspi1){
 int MCP3564_CheckConnection(){
 	//Checks if connected by verifying the mode, accessed through register 0x1
 	//am I able to read something from this?
+	//TxData: CMD byte → CMD[7:6] = 01 (address of device)
+	//		         → CMD[5:2] = 0x1
+	//		         → CMD[1:0] = 01 (static, for now)
+	HAL_StatusTypeDef status;
+
+	unit8_t TxData = 0b01000101; // is this how you do this in C?
+	uint8_t RxData[8]; // create space for status byte
+	uint16_t Size = 8; // Size: 8 bits (is this measured in bits or bytes?)
+	unint32_t Timeout = 100; // this is arbitrary, pls help
+	status = HAL_SPI_TransmitReceive(hspi, &TxData, &RxData, Size, Timeout);
+
+	*dev_addr = ((int32_t)RxData[4] << 4 | (int32_t)RxData[5] << 4); // is the index the bit or the byte?
+	// check that STAT[5:4]=DEV_ADDR[1:0] == 01
+	if (status != HAL_OK) { // function didn't work???
+		return 0;
+	}
+	elif (dev_addr == 01) {
+		return 1; // device connected
+	}
+	return 0;
 }
 
 // Reads voltage calculated from MCP3564, returns 0 if successful, 1 if failed
