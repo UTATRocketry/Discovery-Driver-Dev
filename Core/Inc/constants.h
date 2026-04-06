b66cae0cd8509de189c851c1f826920b43370a12
Prithika Tharan <prithika3226@gmail.com>
2026-04-06 10:31:53 -0400

    fixed baud rate changing functions (kinda)

--- Core/Inc/constants.h
+++ Core/Inc/constants.h
@@ -11,7 +11,7 @@
 #define INC_CONSTANTS_H_
 
 #define GPS_RB_LEN 2048
-#define DMA_LEN 8192
+#define DMA_LEN 256
 #define NMEA_MAX_LEN 256  // using 256 bytes as upper bound for max characters in gga/rmc nmea string
 #define GPS_PROCESS_CHUNK 64 // used in gps_procesS()
 
